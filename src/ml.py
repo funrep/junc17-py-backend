28af250e3324a472b575ce224cfbe73c331f5f8f
alexandertu <alexander.tuoma@gmail.com>
2017-11-25 15:42:42 +0100

    new update

--- src/ml.py
+++ src/ml.py
@@ -58,7 +58,7 @@ clf = SGDClassifier(loss="hinge", penalty="l2")
 clf.fit(X_train, Y_train)
 SGDClassifier(alpha=0.0001, average=False, class_weight=None, epsilon=0.1,
               eta0=0.0, fit_intercept=True, l1_ratio=0.15,
-              learning_rate='optimal', loss='hinge', max_iter=10, n_iter=None,
+              learning_rate='optimal', loss='hinge', max_iter=4, n_iter=None,
               n_jobs=1, penalty='l2', power_t=0.5, random_state=None,
               shuffle=True, tol=None, verbose=0, warm_start=False)
 
--- src/playlist_data.py
+++ src/playlist_data.py
@@ -5,18 +5,22 @@ import json
 import os
 from json.decoder import JSONDecodeError
 
+current_file = input()
+
 
 def get_track_ids(fname):
     tracks = list()
     data = json.load(open(fname))
-    for element in data[0]['items']:
-        #print(element['track']['id'])
-        tracks.append(element['track']['id'])
+    for d in data:
+        for element in d['items']:
+            #print(element['track']['id'])
+            tracks.append(element['track']['id'])
 
+    print(tracks)
     return tracks
 
 username = "alex@mcserver.se"
-track_ids = get_track_ids("lounge.json")
+track_ids = get_track_ids(current_file + ".json")
 track_ids_lounge = get_track_ids
  
 #Temp-fix cache bug
@@ -31,11 +35,16 @@ except(AttributeError, JSONDecodeError):
 if token:
     sp = spotipy.Spotify(auth=token)
 
-    tracks_features = sp.audio_features(track_ids)
+    tracks_features = list()
+
+    steps = len(track_ids) / 40
+    for i in range(0, len(track_ids), 40):
+        tracks_features += sp.audio_features(track_ids[i : max(i, i + 40)])
 
     filedata = json.dumps(tracks_features)
 
-    f = open('lounge_data.json', 'w')
+    f = open(current_file + '_data.json', 'w')
     f.write(filedata)
+    print("Saved file!")
 else:
         print ("Can't get token")
