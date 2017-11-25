52ddb34711eb5cbd67e11d01b726ddee1a28c4b1
Simon Persson <simon@akep.se>
2017-11-25 13:57:18 +0200

    Prepare data for ML

--- src/ml.py
+++ src/ml.py
@@ -1,4 +1,7 @@
-{
+import numpy as np
+import sklearn as sk
+
+tr = {
 "id":"1JI70l1lE5IF2tgJm5TnMD",
    "info":{  
       "album":{  
@@ -211,25 +214,25 @@
          "UY",
          "VN"
       ],
-      "disc_number":1,
-      "duration_ms":162440,
+      "disc_number": 1,
+      "duration_ms": 162440,
       "explicit": False,
-      "external_ids":{  
-         "isrc":"USQX91601142"
+      "external_ids": {  
+         "isrc": "USQX91601142"
       },
-      "external_urls":{  
-         "spotify":"https://open.spotify.com/track/1JI70l1lE5IF2tgJm5TnMD"
+      "external_urls": {  
+         "spotify": "https://open.spotify.com/track/1JI70l1lE5IF2tgJm5TnMD"
       },
-      "href":"https://api.spotify.com/v1/tracks/1JI70l1lE5IF2tgJm5TnMD",
-      "id":"1JI70l1lE5IF2tgJm5TnMD",
-      "name":"Don't Let Me Down - Hardwell & Sephyx Remix",
-      "popularity":61,
-      "preview_url":"https://p.scdn.co/mp3-preview/ceefd8d635958ed2c06d2ea0bfc6dd05950097c1?cid=dc08370fa5224053aedca4f92e85e55a",
-      "track_number":1,
-      "type":"track",
-      "uri":"spotify:track:1JI70l1lE5IF2tgJm5TnMD"
+      "href": "https://api.spotify.com/v1/tracks/1JI70l1lE5IF2tgJm5TnMD",
+      "id": "1JI70l1lE5IF2tgJm5TnMD",
+      "name": "Don't Let Me Down - Hardwell & Sephyx Remix",
+      "popularity": 61,
+      "preview_url": "https://p.scdn.co/mp3-preview/ceefd8d635958ed2c06d2ea0bfc6dd05950097c1?cid=dc08370fa5224053aedca4f92e85e55a",
+      "track_number": 1,
+      "type": "track",
+      "uri": "spotify:track:1JI70l1lE5IF2tgJm5TnMD"
    },
-   "features":[  
+   "features":  
       {  
          "danceability":0.509,
          "energy":0.897,
@@ -250,5 +253,24 @@
          "duration_ms":162440,
          "time_signature":4
       }
-   ]
 }
+
+tracks = list()
+tracks.append(tr)
+
+array = []
+
+def convert_to_array(track):
+    energy = track["features"]["energy"]
+    tempo = track["features"]["tempo"]
+    danceability = track["features"]["danceability"]
+    loudness = track["features"]["loudness"]
+
+    return [energy, tempo, danceability, loudness]
+
+for track in tracks:
+    array.append(convert_to_array(track))
+
+final_array = np.array(array)    
+
+print(final_array)
